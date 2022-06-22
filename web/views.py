b9e026026b3bab9cc4689958ef7cec5d98e0446e
sergeysam27 <sergeysam27@gmail.com>
2022-06-22 21:08:35 +0300

    login and register account

--- web/views.py
+++ web/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+from web.forms import LoginForm, RegisterForm
 
 
 def index(request):
@@ -15,3 +19,46 @@ def contact(request):
 
 def shop(request):
     return render(request, 'web/shop.html')
+
+
+def login_user(request):
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+
+    return render(request, 'web/login.html', context=context)
+
+
+class RegisterView(TemplateView):
+    template_name = 'web/register.html'
+
+
+    def get(self, request):
+        user_form = RegisterForm()
+        context = {'user_form': user_form}
+        return render(request, 'web/register.html', context=context)
+
+
+    def post(self, request):
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            login(request, user)
+            return redirect('index')
+
+        context = {'user_form': user_form}
+        return render(request, 'web/register.html', context=context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login.html')
