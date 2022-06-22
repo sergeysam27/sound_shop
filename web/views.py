ee7fe211930a260eecbde3ac43527412f9c9eeea
sergeysam27 <sergeysam27@gmail.com>
2022-06-22 20:33:08 +0300

    about and contact page

--- web/views.py
+++ web/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     return render(request, 'web/index.html')
 
 
 def about(request):
     return render(request, 'web/about.html')
+
+
+def contact(request):
+    return render(request, 'web/contact.html')
+
+
+def shop(request):
+    return render(request, 'web/shop.html')
